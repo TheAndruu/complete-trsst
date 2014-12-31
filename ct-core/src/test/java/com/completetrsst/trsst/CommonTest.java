8b3c99421ff6217a177bcffd30d388552f9be6a3
acuga <andycuga@gmail.com>
2014-12-30 22:13:43 -0500

    Added testCompile scope simple slf4j dependency for test case logging

--- ct-core/src/test/java/com/completetrsst/trsst/CommonTest.java
+++ ct-core/src/test/java/com/completetrsst/trsst/CommonTest.java
@@ -1,6 +1,6 @@
 package com.completetrsst.trsst;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.security.KeyPair;
 import java.security.PrivateKey;
@@ -8,28 +8,30 @@ import java.security.PublicKey;
 
 import org.apache.commons.codec.binary.StringUtils;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CommonTest {
+	private static final Logger log = LoggerFactory.getLogger(CommonTest.class);
 
 	@Test
 	public void showKeys() {
 		KeyPair pair = Common.generateSigningKeyPair();
 		PublicKey pubKey = pair.getPublic();
-		System.out.println("Format of public key: " + pubKey.getFormat());
+		log.info("Format of public key: " + pubKey.getFormat());
 		assertEquals("X.509", pubKey.getFormat());
-		System.out.println("Algorithm of public key: " + pubKey.getAlgorithm());
-		System.out.println("raw bytes:" + StringUtils.newString(pubKey.getEncoded(), "UTF-8"));
+		log.info("Algorithm of public key: " + pubKey.getAlgorithm());
+		log.info("raw bytes:" + StringUtils.newString(pubKey.getEncoded(), "UTF-8"));
 
 		KeyPair encryptKey = Common.generateEncryptionKeyPair();
 		PrivateKey privateKey = encryptKey.getPrivate();
 
-		System.out.println();
-		System.out.println("Format of private key: " + privateKey.getFormat());
-		System.out.println("Algorithm of private key: " + privateKey.getAlgorithm());
-		System.out.println("raw bytes:" + StringUtils.newString(privateKey.getEncoded(), "UTF-8"));
+		log.info("Format of private key: " + privateKey.getFormat());
+		log.info("Algorithm of private key: " + privateKey.getAlgorithm());
+		log.info("raw bytes:" + StringUtils.newString(privateKey.getEncoded(), "UTF-8"));
 
 		String id = Common.toFeedId(pubKey);
-		System.out.println("This id is: " + id);
+		log.info("This id is: " + id);
 	}
 
 }
