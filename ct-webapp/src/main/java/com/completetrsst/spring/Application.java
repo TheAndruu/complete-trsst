44373537559df710f9a6e3ae8300b907c3e4b925
acuga <andycuga@gmail.com>
2015-01-08 21:11:32 -0500

    Optimizing imports - removing unused entries.

--- ct-webapp/src/main/java/com/completetrsst/spring/Application.java
+++ ct-webapp/src/main/java/com/completetrsst/spring/Application.java
@@ -1,10 +1,8 @@
 package com.completetrsst.spring;
 
-import java.security.Security;
 import java.util.HashMap;
 import java.util.Map;
 
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
