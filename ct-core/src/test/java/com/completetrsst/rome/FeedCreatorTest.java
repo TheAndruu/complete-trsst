8ed82e430df13cc13cf126f5cd5753af8b6b96c6
acuga <andycuga@gmail.com>
2015-01-10 23:26:26 -0500

    Updated rome.properties to have correct package references.  Added test to verify that Feeds have signedXML attached.

--- ct-core/src/test/java/com/completetrsst/rome/FeedCreatorTest.java
+++ ct-core/src/test/java/com/completetrsst/rome/FeedCreatorTest.java
@@ -5,6 +5,7 @@ import static com.completetrsst.crypto.keys.TrsstKeyFunctions.toFeedUrn;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.StringWriter;
 import java.security.KeyPair;
 import java.util.Date;
 import java.util.List;
@@ -15,6 +16,7 @@ import com.completetrsst.crypto.keys.EllipticCurveKeyCreator;
 import com.completetrsst.rome.modules.TrsstModule;
 import com.rometools.rome.feed.atom.Feed;
 import com.rometools.rome.feed.module.Module;
+import com.rometools.rome.io.WireFeedOutput;
 
 public class FeedCreatorTest {
 	private static final KeyPair keyPair = new EllipticCurveKeyCreator().createKeyPair();
@@ -32,5 +34,20 @@ public class FeedCreatorTest {
 		TrsstModule module = (TrsstModule) modules.get(0);
 		assertTrue(module.getIsSigned());
 		assertEquals(keyPair, module.getKeyPair());
+	}
+	
+	@Test
+	public void feedWriting() throws Exception {
+		Feed feed = FeedCreator.createFor(keyPair);
+		WireFeedOutput outputter = new WireFeedOutput();
+		StringWriter writer = new StringWriter();
+		outputter.output(feed, writer);
+		writer.close();
+		String rawXml = writer.toString();
+		assertTrue(rawXml.contains("SignatureValue"));
+		assertTrue(rawXml.contains("http://www.w3.org/2001/04/xmldsig-more#ecdsa-sha1"));
+		String expectedId = toFeedUrn(toFeedId(keyPair.getPublic()));
+		assertTrue(rawXml.contains(expectedId));
+		assertTrue(rawXml.contains("<feed xmlns=\"http://www.w3.org/2005/Atom\">"));
 	}
 }
