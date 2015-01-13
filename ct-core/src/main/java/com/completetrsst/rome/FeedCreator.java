2817b5699f7b84846de4628f8b83631dd2f52666
acuga <andycuga@gmail.com>
2015-01-12 20:35:49 -0500

    Improving FeedCreator - adding helper method to verifySignatures as well as tests.  Unit test actually saved lots of time by pointing out a bug in removal() method.

--- ct-core/src/main/java/com/completetrsst/rome/FeedCreator.java
+++ ct-core/src/main/java/com/completetrsst/rome/FeedCreator.java
@@ -25,71 +25,119 @@ import com.rometools.rome.io.impl.Atom10Generator;
 
 public class FeedCreator {
 
-    private static final Logger log = LoggerFactory.getLogger(FeedCreator.class);
+	private static final Logger log = LoggerFactory.getLogger(FeedCreator.class);
 
-    /**
-     * Creates a feed for the given PublicKey, which determines the feed's ID.
-     * Also has updated date set
-     */
-    public static Feed createFor(KeyPair keyPair) {
-        // Add title?
-        Feed feed = new Feed("atom_1.0");
-        feed.setUpdated(new Date());
-        PublicKey publicKey = keyPair.getPublic();
-        String id = TrsstKeyFunctions.toFeedUrn(TrsstKeyFunctions.toFeedId(publicKey));
-        feed.setId(id);
-        return feed;
+	/**
+	 * Creates a feed for the given PublicKey, which determines the feed's ID.
+	 * Also has updated date set
+	 */
+	public static Feed createFor(KeyPair keyPair) {
+		// Add title?
+		Feed feed = new Feed("atom_1.0");
+		feed.setUpdated(new Date());
+		PublicKey publicKey = keyPair.getPublic();
+		String id = TrsstKeyFunctions.toFeedUrn(TrsstKeyFunctions.toFeedId(publicKey));
+		feed.setId(id);
+		return feed;
+	}
+
+	/**
+	 * Takes a Rome feed to sign, removing all entries prior to signing, so that
+	 * feeds are signed containing only feed-specific elements (no
+	 * entry/content, etc) Re-attaches entries prior to returning signed feed
+	 * element.
+	 */
+	public static Element signFeed(Feed feed, KeyPair keyPair) throws XMLSignatureException {
+		
+		
+		org.jdom2.Element jdomFeed = toJdom(feed);
+
+		// Remove all Entry nodes prior to signing, as required
+		Element domFeed;
+		try {
+			domFeed = XmlUtil.toDom(jdomFeed);
+		} catch (IOException e) {
+			log.error(e.getMessage(), e);
+			throw new XMLSignatureException("Couldn't convert DOM back to Jdom when signing feed", e);
+		}
+		List<Node> removedNodes = removeEntryNodes(domFeed);
+
+		// Actually sign the feed
+		try {
+			SignatureUtil.signElement(domFeed, keyPair);
+		} catch (XMLSignatureException e) {
+			log.error(e.getMessage());
+			throw new XMLSignatureException("Trouble when actually signing the feed", e);
+		}
+
+		// Add the removed entity nodes back
+		addEntries(domFeed, removedNodes);
+
+		// Return the signed feed, with entries added back in
+		return domFeed;
+	}
+
+	static org.jdom2.Element toJdom(Feed feed) throws XMLSignatureException {
+	    Atom10Generator generator = new Atom10Generator();
+		org.jdom2.Element jdomFeed;
+		try {
+			org.jdom2.Document doc = generator.generate(feed);
+			jdomFeed = doc.getRootElement();
+		} catch (FeedException e) {
+			log.error(e.getMessage());
+			throw new XMLSignatureException("Couldn't get feed element as Jdom", e);
+		}
+	    return jdomFeed;
     }
 
-    /**
-     * Takes a Rome feed to sign, removing all entries prior to signing, so that
-     * feeds are signed containing only feed-specific elements (no
-     * entry/content, etc) Re-attaches entries prior to returning signed feed
-     * element.
-     */
-    public static Element signFeed(Feed feed, KeyPair keyPair) throws IOException {
-        Atom10Generator generator = new Atom10Generator();
-        org.jdom2.Element jdomFeed;
-        try {
-            org.jdom2.Document doc = generator.generate(feed);
-            jdomFeed = doc.getRootElement();
-        } catch (FeedException e) {
-            log.error(e.getMessage());
-            throw new IOException(e);
-        }
+	/**
+	 * Verifies the given feed and returns true if the signature validates.
+	 * Returns false if the signature doesn't validate or the content doesn't
+	 * match signed content.
+	 * 
+	 * Verification is done by removing all Atom Entries prior to testing the
+	 * signature. Entries are placed back on the feed element prior to returning
+	 * 
+	 * @throws XmlSignatureException
+	 *             if there is an error parsing the signature, or if none is
+	 *             found
+	 * 
+	 * @param feed
+	 *            The feed to validate, represented as a DOM Element
+	 * @return true if the signature validates the feed, false otherwise
+	 * @throws XmlSignatureException
+	 *             if there's an error encountered parsing or verifying the feed
+	 */
+	public static boolean isVerified(Element domFeed) throws XMLSignatureException {
+		List<Node> removedEntries = removeEntryNodes(domFeed);
+		boolean isVerified = SignatureUtil.verifySignature(domFeed);
+		addEntries(domFeed, removedEntries);
 
-        // Remove all Entry nodes prior to signing, as required
-        Element domFeed = XmlUtil.toDom(jdomFeed);
-        List<Node> removedNodes = removeEntryNodes(domFeed);
+		return isVerified;
+	}
 
-        // Actually sign the feed
-        try {
-            SignatureUtil.signElement(domFeed, keyPair);
-        } catch (XMLSignatureException e) {
-            log.error(e.getMessage());
-            throw new IOException(e);
-        }
+	/** Adds the given entities to the feed */
+	static void addEntries(Element domFeed, List<Node> entityNodesToAdd) {
+		entityNodesToAdd.forEach(entryNode -> domFeed.appendChild(entryNode));
+	}
 
-        // Add back all entry nodes after signing completes
-        removedNodes.forEach(entryNode -> domFeed.appendChild(entryNode));
-
-        // Return the signed feed, with entries added back in
-        return domFeed;
-    }
-
-    /**
-     * Removes all Atom Entry nodes from the given DOM feed element and returns
-     * them as a new list. DOM Feed element is updated in place.
-     */
-    public static List<Node> removeEntryNodes(Element domFeed) {
-        NodeList nodeList = domFeed.getElementsByTagNameNS(SignedEntry.XMLNS, "entry");
-        // we know there's only one entry on this feed
-        List<Node> removedNodes = new ArrayList<Node>();
-        for (int i = 0; i < nodeList.getLength(); i++) {
-            Node node = nodeList.item(i);
-            domFeed.removeChild(node);
-            removedNodes.add(node);
-        }
-        return removedNodes;
-    }
+	/**
+	 * Removes all Atom Entry nodes from the given DOM feed element and returns
+	 * them as a new list. DOM Feed element is updated in place.
+	 */
+	static List<Node> removeEntryNodes(Element domFeed) {
+		NodeList nodeList = domFeed.getElementsByTagNameNS(SignedEntry.XMLNS, "entry");
+		// we know there's only one entry on this feed
+		List<Node> removedNodes = new ArrayList<Node>();
+		int numEntryNodes = nodeList.getLength();
+		for (int i = 0; i < numEntryNodes; i++) {
+			Node node = nodeList.item(i);
+			removedNodes.add(node);
+		}
+		// Need to do the actual removal in a 2nd loop, else as we remove by index,
+		// above, the removals will shift down the location of the next node
+		// thereby not actually removing all the nodes we want
+		removedNodes.forEach(node -> domFeed.removeChild(node));
+		return removedNodes;
+	}
 }
--- ct-core/src/test/java/com/completetrsst/rome/FeedCreatorTest.java
+++ ct-core/src/test/java/com/completetrsst/rome/FeedCreatorTest.java
@@ -11,8 +11,14 @@ import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.jdom2.Text;
 import org.junit.Before;
 import org.junit.Test;
+import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 
@@ -25,72 +31,101 @@ import com.rometools.rome.feed.atom.Feed;
 import com.rometools.rome.io.impl.Atom10Generator;
 
 public class FeedCreatorTest {
-    private static final KeyPair keyPair = new EllipticCurveKeyCreator().createKeyPair();
+	private static final KeyPair keyPair = new EllipticCurveKeyCreator().createKeyPair();
 
-    private static Feed feed;
+	private static Feed feed;
 
-    private static final String ENTRY_TITLE = "foo bar entry title";
+	private static final String ENTRY_TITLE = "foo bar entry title";
 
-    @Before
-    public void init() {
-        feed = FeedCreator.createFor(keyPair);
-        Entry entry = new Entry();
-        entry.setTitle(ENTRY_TITLE);
-        feed.setEntries(Arrays.asList(entry));
-    }
+	@Before
+	public void init() {
+		feed = FeedCreator.createFor(keyPair);
+		Entry entry = new Entry();
+		entry.setTitle(ENTRY_TITLE);
+		feed.setEntries(Arrays.asList(entry));
+	}
 
-    @Test
-    public void createFeedRequiredFields() {
-        String expectedId = toFeedUrn(toFeedId(keyPair.getPublic()));
-        assertEquals(expectedId, feed.getId());
-        assertEquals("atom_1.0", feed.getFeedType());
-        assertTrue(feed.getUpdated().toInstant().isBefore(new Date().toInstant().plusMillis(1L)));
-    }
+	@Test
+	public void createFeedRequiredFields() {
+		String expectedId = toFeedUrn(toFeedId(keyPair.getPublic()));
+		assertEquals(expectedId, feed.getId());
+		assertEquals("atom_1.0", feed.getFeedType());
+		assertTrue(feed.getUpdated().toInstant().isBefore(new Date().toInstant().plusMillis(1L)));
+	}
 
-    @Test
-    public void feedSerializationContainsExpected() throws Exception {
-        Atom10Generator generator = new Atom10Generator();
-        org.jdom2.Document doc = generator.generate(feed);
-        String rawXml = XmlUtil.serializeJdom(doc.getRootElement());
+	@Test
+	public void feedSerializationContainsExpected() throws Exception {
+		Atom10Generator generator = new Atom10Generator();
+		org.jdom2.Document doc = generator.generate(feed);
+		String rawXml = XmlUtil.serializeJdom(doc.getRootElement());
 
-        String expectedId = toFeedUrn(toFeedId(keyPair.getPublic()));
-        assertTrue(rawXml.contains(expectedId));
-        // Entry we added to it
-        assertTrue(rawXml.contains("<feed xmlns=\"http://www.w3.org/2005/Atom\">"));
-        assertTrue(rawXml.contains(ENTRY_TITLE));
-    }
+		String expectedId = toFeedUrn(toFeedId(keyPair.getPublic()));
+		assertTrue(rawXml.contains(expectedId));
+		// Entry we added to it
+		assertTrue(rawXml.contains("<feed xmlns=\"http://www.w3.org/2005/Atom\">"));
+		assertTrue(rawXml.contains(ENTRY_TITLE));
+	}
 
-    @Test
-    public void signedFeedValidates() throws Exception {
-        Element signedFeed = FeedCreator.signFeed(feed, keyPair);
+	@Test
+	public void signedFeedValidates() throws Exception {
+		Element signedFeed = FeedCreator.signFeed(feed, keyPair);
 
-        // Fail -- we want entries removed prior to signature addition
-        assertFalse(SignatureUtil.verifySignature(signedFeed));
+		// Fail -- we want entries removed prior to signature addition
+		assertFalse(SignatureUtil.verifySignature(signedFeed));
 
-        List<Node> removedEntries = FeedCreator.removeEntryNodes(signedFeed);
-        assertTrue(removedEntries.size() > 0);
+		List<Node> removedEntries = FeedCreator.removeEntryNodes(signedFeed);
+		assertTrue(removedEntries.size() > 0);
 
-        // Should pass now
-        assertTrue(SignatureUtil.verifySignature(signedFeed));
-    }
+		// Should pass now
+		assertTrue(SignatureUtil.verifySignature(signedFeed));
+	}
 
-    @Test
-    public void testRemoveEntryNodes() throws Exception {
-        Element signedFeed = FeedCreator.signFeed(feed, keyPair);
-        String rawXmlWithEntries = XmlUtil.serializeDom(signedFeed);
+	@Test
+	public void testRemoveEntryNodes() throws Exception {
+		Element signedFeed = FeedCreator.signFeed(feed, keyPair);
+		String rawXmlWithEntries = XmlUtil.serializeDom(signedFeed);
 
-        // The signed XML has our entry title in it
-        assertTrue(rawXmlWithEntries.contains(ENTRY_TITLE));
+		// The signed XML has our entry title in it
+		assertTrue(rawXmlWithEntries.contains(ENTRY_TITLE));
 
-        List<Node> removedEntries = FeedCreator.removeEntryNodes(signedFeed);
-        rawXmlWithEntries = XmlUtil.serializeDom(signedFeed);
-        // Signed feed no longer contains entry title
-        assertFalse(rawXmlWithEntries.contains(ENTRY_TITLE));
+		List<Node> removedEntries = FeedCreator.removeEntryNodes(signedFeed);
+		rawXmlWithEntries = XmlUtil.serializeDom(signedFeed);
+		// Signed feed no longer contains entry title
+		assertFalse(rawXmlWithEntries.contains(ENTRY_TITLE));
 
-        assertTrue(removedEntries.size() == 1);
-        Element entry = (Element) removedEntries.get(0);
-        Node titleNode = entry.getElementsByTagNameNS(SignedEntry.XMLNS, "title").item(0);
-        assertEquals(ENTRY_TITLE, titleNode.getTextContent());
-    }
+		assertTrue(removedEntries.size() == 1);
+		Element entry = (Element) removedEntries.get(0);
+		Node titleNode = entry.getElementsByTagNameNS(SignedEntry.XMLNS, "title").item(0);
+		assertEquals(ENTRY_TITLE, titleNode.getTextContent());
+	}
 
+	@Test
+	public void testAddEntryNodes() throws Exception {
+		org.jdom2.Element jdomFeed = FeedCreator.toJdom(feed);
+		jdomFeed.addContent(createJdomEntry("first entry"));
+		jdomFeed.addContent(createJdomEntry("second entry"));
+
+		String rawXml = XmlUtil.serializeJdom(jdomFeed);
+		assertTrue(rawXml.contains("first entry"));
+		assertTrue(rawXml.contains("second entry"));
+		assertTrue(rawXml.contains(ENTRY_TITLE));
+		
+		Element domFeed = XmlUtil.toDom(jdomFeed);
+		List<Node> removedNodes = FeedCreator.removeEntryNodes(domFeed);
+
+		rawXml = XmlUtil.serializeDom(domFeed);
+		assertFalse(rawXml.contains("first entry"));
+		assertFalse(rawXml.contains("second entry"));
+		assertFalse(rawXml.contains(ENTRY_TITLE));
+		
+		// 3 nodes = 2 we added in this test and original with ENTRY_TITLE in it
+		assertEquals(3, removedNodes.size());
+		removedNodes.forEach(node -> assertTrue(node.getFirstChild().getTextContent().contains("entry")));
+	}
+
+	private org.jdom2.Element createJdomEntry(String text) {
+		org.jdom2.Element entry = new org.jdom2.Element("entry", SignedEntry.XMLNS);
+		entry.addContent(new Text(text));
+		return entry;
+	}
 }
